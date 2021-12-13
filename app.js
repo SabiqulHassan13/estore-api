ea5c2193ee688312cf4fd9a6342d41d65957bf07
SabiqulHassan13 <sabiqulhassan98@gmail.com>
2021-12-14 01:36:03 +0600

    set the db connection

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 
-const sequelize = require("./utils/db-sequelize");
+const db = require("./utils/db-sequelize");
 const userRoutes = require("./routes/user-routes");
 // const productRoutes = require("./routes/product-routes");
 
@@ -10,11 +10,14 @@ const Product = require("./models/product-model");
 
 const app = express();
 
-// db connection
-sequelize
-  .sync()
-  .then((result) => console.log(result))
-  .catch((err) => console.log(err));
+// db connection check
+// sequelize
+//   .sync()
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err));
+db.authenticate()
+  .then(() => console.log("Database connected successfully..."))
+  .catch((err) => console.log("Database connection failed: " + err));
 
 // middleware list
 app.use(express.json());
@@ -23,7 +26,7 @@ app.use(express.json());
 app.use("/api/users", userRoutes);
 // app.use("/api/products", productRoutes);
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
 });
