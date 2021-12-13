45db546263cc29b7a4f0e652988435b6fe1d7e0f
SabiqulHassan13 <sabiqulhassan98@gmail.com>
2021-12-13 21:34:12 +0600

    added id login to create user

--- controllers/api/user-controller.js
+++ controllers/api/user-controller.js
@@ -1,7 +1,7 @@
 const USER_LIST = [];
 
 exports.fetchUserList = (req, res) => {
-  return res.status(200).json("fetch user list");
+  return res.status(200).json(USER_LIST);
 };
 
 exports.fetchUserDetail = (req, res) => {
@@ -14,6 +14,7 @@ exports.createUser = (req, res) => {
   const { username, email, password, c_password } = req.body;
 
   const newUser = {
+    id: USER_LIST.length + 1,
     username,
     email,
     password,
