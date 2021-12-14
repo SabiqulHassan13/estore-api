48f04589fea60dfd0dd3835117e8d6f8f56e3afd
SabiqulHassan13 <sabiqulhassan98@gmail.com>
2021-12-14 17:59:44 +0600

    added product update & delete routes

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -46,3 +46,19 @@ exports.findProductById = async (req, res) => {
     return res.status(500).json({ message: err.message });
   }
 };
+
+exports.updateProductById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const product = await Product.update(req.body, { where: { id } });
+
+    if (product == 1) {
+      return res.status(200).json({ message: "Product updated successfully" });
+    } else {
+      return res.status(404).json({ message: "Product not found" });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
